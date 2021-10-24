65788bf143ce0bf761bb9b368215de53059f2821
wwwonka <je.mange.du.boeuf@gmail.com>
2021-10-24 17:39:18 -0400

    Added an input form that displays currency

---
+++ patnais/payment/build/js/main.js
@@ -0,0 +1,6 @@
+(() => {
+  // src/main.js
+  var input = "input";
+  var options = {};
+  var cinput = new CurrencyInput(input, options);
+})();
---
+++ patnais/payment/src/main.js
@@ -0,0 +1,7 @@
+const input = 'input';
+const options = {
+	// currency: 'EUR',
+	// locale: 'de'
+	//Options can be partially or completely omitted.
+}
+const cinput = new CurrencyInput(input, options);
